b8e03ed689cfb2b24a27ee471c9b7754408c4b92
yvesgarciaespinosa <yvesgarciaespinosa@gmail.com>
2022-09-12 10:51:27 -0500

    Examen finalizado

---
+++ app/Http/Controllers/TareasController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Tarea;
+use App\models\Empresa;
+use App\models\User;
+
+
+class TareasController extends Controller
+{
+    public function mostrarPendientes(){
+        $tareas = Tarea::where('estatus','Pendiente')
+        ->select('tareas.nombre','tareas.descripcion','tareas.fecha_inicio','tareas.fecha_vencimiento', 'empresas.nombre as empresa','users.name as usuario')
+        ->join('empresas','tareas.id_empresa','=','empresas.id')
+        ->join('users','tareas.id_usuario','=','users.id')
+        ->orderBy('empresa')
+        ->get();
+
+        foreach($tareas as $tarea){
+            if($tareas->usuario == null){
+                $tareas->usuario = "Sin usuario asignado";
+            }
+        }
+        return response()->json($tareas);
+    }
+
+    public function usuariosDisponibles(){
+
+        $empresasTareas = [];
+
+        $empresas = Empresa::all();
+
+        foreach($empresas as $empresa){
+            $tareas = Tarea::where('estatus','Pendiente')->where('id_empresa',$empresa->id);
+            $empresa = ["nombre" => $empresa->nombre, "cantidad" => $tareas->count()];
+
+            array_push($empresasTareas,$empresa);
+        }
+
+        $empresaMasPendientes = ["nombre" => null, "cantidad" => 0];
+        $max = 0;
+        foreach($empresasTareas as $empresa){
+            if($empresa["cantidad"] > $max){
+                $empresaMasPendientes = $empresa;
+                $max = $empresa["cantidad"];
+            }
+        }
+
+
+        $usuariosTareas = [];
+
+        $usuarios = User::all();
+
+        foreach($usuarios as $usuario){
+            $tareas = Tarea::where('estatus','Pendiente')->where('id_usuario',$usuario->id);
+            $usuario = ["nombre" => $usuario->name, "cantidad" => $tareas->count()];
+
+            array_push($usuariosTareas,$usuario);
+        }
+
+        $usuariosDisponibles = [];
+
+        foreach($usuariosTareas as $usuario){
+            if($usuario["cantidad"] < 5){
+                array_push($usuariosDisponibles,$usuario);
+            }
+        }
+
+        $empresaMasPendientes = json_encode($empresaMasPendientes);
+        $usuariosDisponibles = json_encode($usuariosDisponibles);
+
+        return "Empresa con mas tareas pendientes: " . $empresaMasPendientes . " Usuarios sin maximo de tareas: " . $usuariosDisponibles;
+
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Empresa;
+use App\Models\Tarea;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,266 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->name = 'Yves Garcia';
+        $user->email = 'admin@netcommerce.com';
+        $user->password = bcrypt('Pass123.');
+        $user->save();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = new User();
+        $user->name = 'Alberto Gómez';
+        $user->email = 'admin1@netcommerce.com';
+        $user->password = bcrypt('Pass123.');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Ezequiel Segura';
+        $user->email = 'admin2@netcommerce.com';
+        $user->password = bcrypt('Pass123.');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Adrian Araya';
+        $user->email = 'admin3@netcommerce.com';
+        $user->password = bcrypt('Pass123.');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Rafa Meza';
+        $user->email = 'admin4@netcommerce.com';
+        $user->password = bcrypt('Pass123.');
+        $user->save();
+
+        $empresa = new Empresa();
+        $empresa->nombre = 'Disney';
+        $empresa->save();
+
+        $empresa = new Empresa();
+        $empresa->nombre = 'Sony';
+        $empresa->save();
+
+        $empresa = new Empresa();
+        $empresa->nombre = 'Microsoft';
+        $empresa->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '1';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '3';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '3';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '4';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '4';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '4';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '4';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '4';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '3';
+        $tarea->id_usuario = '5';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '3';
+        $tarea->id_usuario = '5';
+        $tarea->estatus = 'Pendiente';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '1';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '1';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '1';
+        $tarea->id_usuario = '2';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '3';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '2';
+        $tarea->id_usuario = '4';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
+
+        $tarea = new Tarea();
+        $tarea->nombre = 'Actividad';
+        $tarea->descripcion = 'Descripción de la actividad';
+        $tarea->id_empresa = '3';
+        $tarea->id_usuario = '5';
+        $tarea->estatus = 'Completada';
+        $tarea->fecha_inicio = '08-09-2022';
+        $tarea->fecha_vencimiento = '10-09-2022';
+        $tarea->save();
     }
 }
